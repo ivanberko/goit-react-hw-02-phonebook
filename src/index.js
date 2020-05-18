9760101ece569e2e76cd0cbc3fc891fc5d1a02a0
Ivan Berk <berkoivan@gmail.com>
2020-05-19 00:43:27 +0300

    install uuid

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { v4 as uuidv4 } from "uuid";
 import App from "./Components/App/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
